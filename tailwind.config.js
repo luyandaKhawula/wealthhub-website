1ea017593e45a17b6c09b5c4b9c272548f9ff627
Luyanda <luyandakwazi16@gmail.com>
2025-09-11 09:58:33 +0200

    MyWealthHub Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gold: '#C8A349',
+        charcoal: '#1C1C1C',
+        light: '#F8F8F8',
+        textdark: '#222222',
+      }
+    }
+  }
+}
